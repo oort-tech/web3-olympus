bed690a526cf7d5c845229b7de33b09bbb9bdab6
catlina <catlina@computecoin.network>
2022-11-22 13:44:36 +0800

    fix debug unit test

--- test/debug.test.ts
+++ test/debug.test.ts
@@ -16,9 +16,8 @@ describe("web3-olympus debug test", function() {
         hash: "0x46b037fe3043b4b27f05fec9db42eec8093673a788408264ea7a8e27fd38fb37",
         account: "0x7A045fB4388C9282D0ef73C97CE92ebcaAFeC550",
         begin: "0x0000000000000000000000000000000000000000",
-        max_results: 100
+        maxResults: 100
       });
-      console.log(result);
       expect(result.code).toBe(0);
     });
   });
