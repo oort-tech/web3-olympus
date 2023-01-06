3b2de1a1e70afbecc10fdaae1a734244a6268603
fate <fatestylelove@outlook.com>
2023-01-06 15:10:45 +0800

    feat: update rpc

--- test/epoch.test.ts
+++ test/epoch.test.ts
@@ -1,23 +1,20 @@
-import Olympus from "../lib";
+import Olympus from '../lib';
 
-const provider = "https://beta-rpc.mainnet.computecoin.com";
+const provider = 'https://beta-rpc.mainnet.computecoin.com';
 
 const olympusRequest = new Olympus(provider);
 
-describe("web3-olympus epoch test", function() {
-  describe("#epoch", function() {
-    it("Should return epoch approves.", async () => {
-      const result = await olympusRequest.epochApproves(10);
-      expect(result.code).toBe(0);
+describe('web3-olympus epoch test', function () {
+  describe('#epoch', function () {
+    let epochApprovesResult;
+    it('Should return epoch approves.', async () => {
+      epochApprovesResult = await olympusRequest.epochApproves(1);
+      expect(epochApprovesResult.code).toBe(0);
     });
 
-    it("Should return epoch approve receipts.", async () => {
-      const result = await olympusRequest.epochApproveReceipts(10);
-      expect(result.code).toBe(0);
-    });
-
-    it("Should return epoch elected approve receipts.", async () => {
-      const result = await olympusRequest.epochElectedApproveReceipts(10);
+    it('Should return approve receipt.', async () => {
+      const hash = epochApprovesResult.result[0].hash;
+      const result = await olympusRequest.approveReceipt(hash);
       expect(result.code).toBe(0);
     });
   });
