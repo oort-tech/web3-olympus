ebb281abe60be577d7b609dbfffa809b66a745d2
fate <fatestylelove@outlook.com>
2023-03-01 17:19:43 +0800

    🔥  Fix docs and deprecate interfaces

--- test/block.test.ts
+++ test/block.test.ts
@@ -1,47 +1,37 @@
-import Olympus from "../lib";
+import Olympus from '../lib';
 
-const provider = "https://beta-rpc.mainnet.computecoin.com";
+const provider = 'https://beta-rpc.mainnet.computecoin.com';
 
 const olympusRequest = new Olympus(provider);
 
-describe("web3-olympus block test", function() {
-  describe("#block", function() {
-    it("Should return block info.", async () => {
-      const result = await olympusRequest.block(
-        "0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77"
-      );
+describe('web3-olympus block test', function () {
+  describe('#block', function () {
+    it('Should return block info.', async () => {
+      const result = await olympusRequest.block('0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77');
       expect(result.code).toBe(0);
     });
 
-    it("Should return block state.", async () => {
-      const result = await olympusRequest.blockState(
-        "0xdf986339ff2f1ccb6bf59e7efd46bfd84fb0a518be14dd897f3e6ed54aa15e8f"
-      );
+    it('Should return block state.', async () => {
+      const result = await olympusRequest.blockState('0xdf986339ff2f1ccb6bf59e7efd46bfd84fb0a518be14dd897f3e6ed54aa15e8f');
       expect(result.code).toBe(0);
     });
 
-    it("Should return block states.", async () => {
-      const result = await olympusRequest.blockStates([
-        "0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77"
-      ]);
+    it('Should return block states.', async () => {
+      const result = await olympusRequest.blockStates(['0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77']);
       expect(result.code).toBe(0);
     });
 
-    it("Should return block traces.", async () => {
-      const result = await olympusRequest.blockTraces(
-        "0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77"
-      );
+    it('Should return block traces.', async () => {
+      const result = await olympusRequest.blockTraces('0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77');
       expect(result.code).toBe(0);
     });
 
-    it("Should return block summary.", async () => {
-      const result = await olympusRequest.blockSummary(
-        "0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77"
-      );
+    it('Should return block summary.', async () => {
+      const result = await olympusRequest.blockSummary('0x8134d032b0a9cdbe1d63c33d5236279f0b13f04164b7d04fb03d9a0e63f3ba77');
       expect(result.code).toBe(0);
     });
 
-    it("Should return stable blocks.", async () => {
+    it('Should return stable blocks.', async () => {
       const result = await olympusRequest.stableBlocks(10, 12345);
       expect(result.code).toBe(0);
     });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,32 +1,32 @@
-import Olympus from "../lib";
+import Olympus from '../lib';
 
-const provider = "https://beta-rpc.mainnet.computecoin.com";
+const provider = 'https://beta-rpc.mainnet.computecoin.com';
 
 const olympusRequest = new Olympus(provider);
 
-describe("web3-olympus block chain test", function() {
-  describe("#block chain", function() {
-    it("Should return block chain status.", async () => {
+describe('web3-olympus block chain test', function () {
+  describe('#block chain', function () {
+    it('Should return block chain status.', async () => {
       const result = await olympusRequest.status();
       expect(result.code).toBe(0);
     });
 
-    it("Should return block chain version.", async () => {
+    it('Should return block chain version.', async () => {
       const result = await olympusRequest.version();
       expect(result.code).toBe(0);
     });
 
-    it("Should return block chain peers.", async () => {
+    it('Should return block chain peers.', async () => {
       const result = await olympusRequest.peers();
       expect(result.code).toBe(0);
     });
 
-    it("Should return block chain nodes.", async () => {
+    it('Should return block chain nodes.', async () => {
       const result = await olympusRequest.nodes();
       expect(result.code).toBe(0);
     });
 
-    it("Should return block chain witness list.", async () => {
+    it('Should return block chain witness list.', async () => {
       const result = await olympusRequest.witnessList();
       expect(result.code).toBe(0);
     });
